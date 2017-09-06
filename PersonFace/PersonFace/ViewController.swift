01434f101662e90716820ba3e4fced63e7c644a8
Sifiso Mazibuko <SifisoMazibuko@users.noreply.github.com>
2017-09-06 10:17:28 +0200

    Initial Commit

---
+++ PersonFace/PersonFace/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PersonFace
+//
+//  Created by Sifiso Mazibuko on 2017/09/06.
+//  Copyright © 2017 Sifiso Mazibuko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
